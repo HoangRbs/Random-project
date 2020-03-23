fe17be42d109f154871f9b512cb641450ccce486
HoangVy <hoangrbs@gmail.com>
2020-03-23 09:36:21 +0700

    edit db connection

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ require('dotenv').config({
 });
 const mongoose = require('mongoose');
 
+const DB_URL =
+  process.env.NODE_ENV === 'development'
+    ? 'mongodb://localhost/random-project'
+    : `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0-ju1lb.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
 mongoose
-  .connect(process.env.DATABASE_URL, {
+  .connect(DB_URL, {
     useNewUrlParser: true
   })
   .then(() => console.log('database connected'));
