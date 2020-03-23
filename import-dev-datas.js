4870a21654b91c5a097ab72fe8e658a4a61e5cfd
HoangVy <hoangrbs@gmail.com>
2020-03-23 09:58:33 +0700

    edit readme

--- import-dev-datas.js
+++ import-dev-datas.js
@@ -2,10 +2,22 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 const Tour = require('./models/Tour');
 
+require('dotenv').config({
+  path: `./config.env`
+});
+
+const DB_URL =
+  process.env.NODE_ENV === 'development'
+    ? 'mongodb://localhost/random-project'
+    : `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0-ju1lb.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
 mongoose
-  .connect('mongodb://localhost/random-project', {
-    useNewUrlParser: true
-  })
+  .connect(
+    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0-ju1lb.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true
+    }
+  )
   .then(() => console.log('database connected'));
 
 let datas = fs.readFileSync(`${__dirname}/dev-data/tours.json`);
--- readme.md
+++ readme.md
@@ -1,1 +1,8 @@
 documentation: https://documenter.getpostman.com/view/7985991/SzS2wTWP?version=latest
+demo-links: https://random-project.herokuapp.com/
+
+INSTALLING:
+
+- npm run load-dev-datas to load all the tours into database
+- npm run start:dev
+- localhost:3001
